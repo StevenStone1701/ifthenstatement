219ba781c405ae470ddac39e4d9bdc02131d1cdb
StevenStone1701 <34069326+StevenStone1701@users.noreply.github.com>
2018-01-24 14:50:44 +0100

    Initial commit

---
+++ Module1.vb
@@ -0,0 +1,20 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Alice As String = "allice"
+        Dim Steven As String = "steven"
+        Console.Write("what's your name?: ")
+        Dim userValue As String = Console.ReadLine()
+        If LCase(userValue) = Alice Then
+            Console.WriteLine("Hi Allice.")
+        ElseIf LCase(userValue) = Steven Then
+            Console.WriteLine("Hi Steven.")
+        Else
+            Console.WriteLine("Sorry, I don't know you.")
+        End If
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
